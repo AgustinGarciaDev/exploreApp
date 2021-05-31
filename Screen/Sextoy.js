570e5de42c2b7250ab05454f2b64c3855c73c75f
dupuyivan <dupuyivan8@gmail.com>
2021-05-30 22:16:26 -0300

    actualizaciones

---
+++ Screen/Sextoy.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+const Sextoy = ()=>{
+
+
+return<View>
+    <Text>Sextoy</Text>
+</View>  
+}
+
+
+
+
+export default Sextoy
--- redux/Reducer/mainReducer.js
+++ redux/Reducer/mainReducer.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux'
 import cartReducer from './cartReducer'
 import userReducer from './userReducer'
-import productReducer from './productReducer'
+import productsReducer from './productReducer'
 
 const mainReducer = combineReducers({
     user: userReducer,
-    /*  cart: cartReducer,
-     productReducer */
+     /* cart: cartReducer, */
+     productsReducer
 })
 
 export default mainReducer
