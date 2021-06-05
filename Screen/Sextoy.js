8a08cf47d96c0d21a96fa688884049fd34f6769c
dupuyivan <dupuyivan8@gmail.com>
2021-06-05 00:00:04 -0300

    antes del pulleo

--- Screen/Sextoy.js
+++ Screen/Sextoy.js
@@ -1,15 +1,43 @@
-import React from "react"
-import { View, Text } from "react-native"
+import React,{ useEffect,useState } from "react"
+import { connect } from "react-redux"
+import productsActions from "../redux/Action/productsActions"
+import { StyleSheet, ScrollView, View, Text } from "react-native"
+import CardProduct from "../Components/products/CardProduct"
 
-const Sextoy = ()=>{
+const Sextoy = ({ navigation, allProducts, fetchAllProducts })=>{
+    
+    useEffect(()=>{
+        !allProducts.length &&
+        fetchAllProducts()
+        console.log( allProducts.filter( product => product.categories.filter( categori=> categori.name === "sextoy" ) ) ) 
+    },[])
 
+    console.log( allProducts )
 
-return<View>
+return<ScrollView>
     <Text>Sextoy</Text>
-</View>  
+    {   allProducts.length 
+        ? allProducts.map( product => <CardProduct navigation={navigation} key={product._id} product={product} />)
+        
+        :<Text>Don't have any articles</Text>
+    }
+
+</ScrollView>  
+}
+
+const mapStateToprops = state =>{
+    return{
+        allProducts:state.productsReducer.allProducts
+    }
+}
+
+const mapDispatchToProps = {
+    fetchAllProducts:productsActions.fetchAllProducts
 }
 
 
+export default connect(mapStateToprops,mapDispatchToProps) (Sextoy)
 
+const styles = StyleSheet.create({
 
-export default Sextoy
+})
